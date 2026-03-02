a0c6175f8aabddca4c741a525f2c5003a349e654
JustZaza <zandemnothoza@gmail.com>
2026-03-02 11:09:07 +0200

    Simple Calculator - Java

---
+++ SimpleCalc/src/simplecalc/SimpleCalc.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package simplecalc;
+
+/**
+ *
+ * @author Azande
+ */
+public class SimpleCalc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
